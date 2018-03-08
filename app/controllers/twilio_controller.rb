fd32fe11a3663f1406e01b3e09c85928583b9a37
Yechiel Kalmenson <contact@yechiel.me>
2018-03-07 21:46:16 -0500

    add logic to subscribe users

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,12 @@
 class TwilioController < ApplicationController
   def sms
+    response = helpers.parse_sms(params)
+
+    text = Twilio::TwiML::MessagingResponse.new do |r|
+      r.message body: response
+    end
+
+    render xml: text.to_s
   end
 
   def voice
--- app/helpers/twilio_helper.rb
+++ app/helpers/twilio_helper.rb
@@ -1,2 +1,30 @@
 module TwilioHelper
+
+    def parse_sms(sms)
+
+        body = sms[:Body]&.upcase
+        from = sms[:From]
+
+        case true
+        when body.include?("UNSUBSCRIBE")
+
+        when body.include?("SUBSCRIBE")
+            subscriber = Subscriber.new(number: from)
+            if subscriber.save
+                "#The number #{from} has been subscribed to recieve alerts. Text UNSUBSCRIBE at any time to unsubscribe."
+            else
+                "The number #{from} has not been saved."
+            end
+
+        when body.include?("STATUS")
+
+        else
+
+        end
+            
+
+    end
+
+    private
+
 end
