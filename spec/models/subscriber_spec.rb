036820f4e91a6af9c8672f08094cce42fef56d3e
Yechiel Kalmenson <contact@yechiel.me>
2018-03-07 19:28:46 -0500

    add more model tests for subscriber

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -4,18 +4,20 @@ RSpec.describe Subscriber, type: :model do
 
   @valid_number = "+1234567890"
   let(:subscriber) {Subscriber.create(number: @number)}
+  let(:no_number_subscriber) {Subscriber.create(number: @number)}
 
   @invalid_number = "invalid_number"
-  let(:invalid_subscriber) {Subscriber.create(number: @invalid_number)}
+  let(:invalid_number_subscriber) {Subscriber.create(number: @invalid_number)}
 
   it "has a phone number" do
+    expect(no_number_subscriber).not_to be_valid
     expect(subscriber.number).to eq(@valid_number)
   end
 
   describe "phone number" do
 
     it "is valid" do
-      expect(invalid_subscriber).not_to be_valid
+      expect(invalid_number_subscriber).not_to be_valid
     end
 
   end
