6e6e9ff62a3401b27e294c8ac815cee8c859ae5c
Yechiel Kalmenson <contact@yechiel.me>
2018-03-09 15:10:41 -0500

    add specs to voice request to ensure the returned status is sanitized

--- spec/requests/voice_request_spec.rb
+++ spec/requests/voice_request_spec.rb
@@ -2,15 +2,23 @@ require "rails_helper"
 
 RSpec.describe "Voice request cycle", :type => :request do
 
-    before(:each) {@status = Status.create(body: "Test Status")}
+    before(:each) do
+        @status = Status.create(body: "#NYCASP are in effect")
+        get '/twilio/voice'
+
+    end
 
    
     it "responds with a twiml containing the latest status" do
-        get '/twilio/voice'
 
         expect(response.content_type).to eq('application/xml')
         expect(response.body).to include(@status.body)
 
+    end
+
+    it "ensure the reply is sanitized for voice" do
+        expect(response.body).not_to include("#NYCASP")
+        expect(response.body).to include("New York City Alternate Side Parking")
     end
 
 end
