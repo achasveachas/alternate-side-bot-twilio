cf55caf6a865c277d436f6fbaca8800090c8f529
Yechiel Kalmenson <contact@yechiel.me>
2018-03-08 23:04:00 -0500

    add authentication logic to status controller

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,4 +1,15 @@
 class StatusController < ApplicationController
+  before_action :authenticate
+
   def status
+    @status = Status.new(status_params)
+
+
+  end
+
+  private
+
+  def authenticate
+    head 403 unless request.env['HTTP_AUTHORIZATION']&.split(" ")&.last == ENV['AUTH_CODE']
   end
 end
