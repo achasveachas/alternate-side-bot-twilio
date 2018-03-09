256521ebabd5cef6d2d4b9328c3763f80321952a
Yechiel Kalmenson <contact@yechiel.me>
2018-03-09 14:58:19 -0500

    add spec to make sure status has a body

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -4,9 +4,16 @@ RSpec.describe Status, type: :model do
 
   let(:suspended_status) {Status.create(body: "Alternate side parking rules are suspended", suspended: true)}
   let(:status_in_effect) {Status.create(body: "Alternate side parking rules are in effect")}
+  let(:no_body_status) {Status.create(body: "")}
 
-  it "has a body" do
-    expect(suspended_status.body).not_to be_empty
+  describe "body" do
+    it "has a body" do
+      expect(suspended_status.body).not_to be_empty
+    end
+
+    it "is invalid without a body" do
+      expect(no_body_status).not_to be_valid
+    end
   end
 
   describe "suspended" do
