50ef2eadee493d3ee1fb44c0cfa7964858ce571b
Yechiel Kalmenson <contact@yechiel.me>
2018-03-08 23:01:50 -0500

    fix the status request specs

--- spec/requests/status_request_spec.rb
+++ spec/requests/status_request_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 RSpec.describe "Status request cycle", :type => :request do
 
-    auth_code = ENV['AUTH_CODE']
-    @authorized_headers = {authorization: auth_code}
-    @suspended_status = {status: {body: "Rules are suspended", suspended: true}}
-    @not_suspended_status = {status: {body: "Rules are not suspended", suspended: false}}
+    before(:each) do
+        @auth_code = ENV['AUTH_CODE']
+        @authorized_headers = {"Authorization": "Bearer #{@auth_code}"}
+        @suspended_status = {status: {body: "Rules are suspended", suspended: true}}
+        @not_suspended_status = {status: {body: "Rules are not suspended", suspended: false}}
+    end
 
     describe "Authentication" do
    
         it "accepts authenticated requests" do
-            post '/status', @suspended_status, @authorized_headers
+            post '/status', params: @suspended_status, headers: @authorized_headers
 
             expect(response.status).to eq(204)
         end
@@ -26,10 +28,10 @@ RSpec.describe "Status request cycle", :type => :request do
 
     describe "POST /status" do
         it "creates a new status" do
-            post '/status', @suspended_status, @authorized_headers
+            post '/status', params: @suspended_status, headers: @authorized_headers
             expect(Status.last.body).to eq(@suspended_status[:status][:body])
 
-            post '/status', @not_suspended_status, @authorized_headers
+            post '/status', params: @not_suspended_status, headers: @authorized_headers
             expect(Status.last.body).to eq(@not_suspended_status[:status][:body])
         end
     end
