e2e9dc63ceaf7d4c6349d4cd4c5c0e2feeb47c0a
Yechiel Kalmenson <contact@yechiel.me>
2018-03-09 15:10:20 -0500

    fix the status specs

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Status, type: :model do
 
-  let(:suspended_status) {Status.create(body: "Alternate side parking rules are suspended", suspended: true)}
-  let(:status_in_effect) {Status.create(body: "Alternate side parking rules are in effect")}
+  let(:suspended_status) {Status.create(body: "#NYCASP rules are suspended", suspended: true)}
+  let(:status_in_effect) {Status.create(body: "#NYCASP are in effect")}
   let(:no_body_status) {Status.create(body: "")}
 
   describe "body" do
@@ -24,6 +24,13 @@ RSpec.describe Status, type: :model do
 
     it "defaults to false" do
       expect(status_in_effect.suspended).to be false
+    end
+  end
+
+  describe "to_speech" do
+    it "parses the body to be more voice friendly" do
+      expect(suspended_status.to_speech).not_to include("#NYCASP")
+      expect(suspended_status.to_speech).to include("New York City Alternate Side Parking")
     end
   end
 
