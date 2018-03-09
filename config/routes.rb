3f1452436dd03b6a5d37bc1014eb0156a4c076c0
Yechiel Kalmenson <contact@yechiel.me>
2018-03-08 20:54:44 -0500

    change the voice route to a get

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post 'twilio/sms'
 
-  post 'twilio/voice'
+  get 'twilio/voice'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
