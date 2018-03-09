f03154ceb5d6c0a81cbebeaf584d20517061a20c
Yechiel Kalmenson <contact@yechiel.me>
2018-03-08 21:06:00 -0500

    fix up the voice specs

--- spec/requests/voice_request_spec.rb
+++ spec/requests/voice_request_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe "Voice request cycle", :type => :request do
 
-    let(:status) {Status.new(body: "Test Status")}
+    before(:each) {@status = Status.create(body: "Test Status")}
 
    
     it "responds with a twiml containing the latest status" do
         get '/twilio/voice'
 
         expect(response.content_type).to eq('application/xml')
-        expect(response.body).to include(status.body)
+        expect(response.body).to include(@status.body)
 
     end
 
