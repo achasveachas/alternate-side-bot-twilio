699a57e0dfdbac6cee718fd09697f0f4692cbeeb
Yechiel Kalmenson <contact@yechiel.me>
2018-03-11 01:10:30 -0500

    configure tweetstreem

---
+++ config/initializers/tweetstream.rb
@@ -0,0 +1,7 @@
+TweetStream.configure do |config|
+    config.consumer_key        = ENV["CONSUMER_KEY"]
+    config.consumer_secret     = ENV["CONSUMER_SECRET"]
+    config.oauth_token        = ENV["ACCESS_TOKEN"]
+    config.oauth_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    config.auth_method        = :oauth
+  end
