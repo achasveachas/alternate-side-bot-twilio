8d4e25e8798bf3989484f53be4a197f4836706a6
Yechiel Kalmenson <contact@yechiel.me>
2018-03-11 00:18:02 -0500

    change the voice of the voice message

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -12,9 +12,9 @@ class TwilioController < ApplicationController
   def voice
 
     response = Twilio::TwiML::VoiceResponse.new do |r|
-      r.say Status.last.to_speech
+      r.say Status.last.to_speech, voice: 'alice'
       r.pause langth: 1
-      r.say "To subscribe to get SMS messages whenever rules are suspended, text 'sunscribe' to this number"
+      r.say "To subscribe to get SMS messages whenever rules are suspended, text 'sunscribe' to this number", voice: 'alice'
     end
 
     render xml: response.to_s
