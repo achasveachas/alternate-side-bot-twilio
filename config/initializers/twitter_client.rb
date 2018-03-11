750530f5de3ea182bd08e79e6287b34e36be2c87
Yechiel Kalmenson <contact@yechiel.me>
2018-03-11 11:37:15 -0400

    configure twitter client

--- config/initializers/twitter_client.rb
+++ config/initializers/twitter_client.rb
@@ -1,6 +1,6 @@
 Rails.configuration.twitter = Twitter::Client.new do |config|
-    config.consumer_key        = ENV["CONSUMER_KEY"]
-    config.consumer_secret     = ENV["CONSUMER_SECRET"]
-    config.access_token        = ENV["ACCESS_TOKEN"]
-    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 end
