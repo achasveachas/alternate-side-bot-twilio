9456acf1b2472f664cfda2c4f374adaf122df9fb
Yechiel Kalmenson <contact@yechiel.me>
2018-08-01 21:26:00 -0400

    refactor subscriber model

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,6 +2,6 @@ class Subscriber < ApplicationRecord
     validates :number, presence: true, uniqueness: true
 
     def whatsapp_subscriber?
-        self.number.start_with?("whatsapp:")
+        number.start_with?("whatsapp:")
     end
 end
