c7d250f9623853918b926caa31d97528d2059656
Yechiel Kalmenson <contact@yechiel.me>
2018-08-01 21:37:20 -0400

    add custom number format validator to account for whatsapp numbers

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,7 +1,14 @@
 class Subscriber < ApplicationRecord
     validates :number, presence: true, uniqueness: true
+    validate :number_format
 
     def whatsapp_subscriber?
-        number.start_with?("whatsapp:")
+        number.start_with? "whatsapp:"
+    end
+
+    def number_format
+        unless /\A\+\d{7,15}\z/.match(number) || /\Awhatsapp:\+\d{7,15}\z/.match(number)
+            errors.add :number, "is the wrong format"
+        end
     end
 end
