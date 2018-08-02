13f15c3f22da1a69c8549e630a0e27a8894e1606
Yechiel Kalmenson <contact@yechiel.me>
2018-08-01 21:19:49 -0400

    get the whatsapp_subscriber? method working

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,6 +2,6 @@ class Subscriber < ApplicationRecord
     validates :number, presence: true, uniqueness: true
 
     def whatsapp_subscriber?
-        true
+        self.number.start_with?("whatsapp:")
     end
 end
