9c9ca302f44dad6316edff81d771fd0e3a0b3be8
Yechiel Kalmenson <contact@yechiel.me>
2018-08-01 21:14:30 -0400

    add test for whatsapp subscription

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
 
-  let(:subscriber) {Subscriber.create(number: @valid_number)}
-  let(:whatsapp_subscriber) {Subscriber.create(number: "invalid_number")}
+  let(:subscriber) {Subscriber.create(number: "+123456789")}
+  let(:whatsapp_subscriber) {Subscriber.create(number: "whatsapp+123456789")}
   let(:no_number_subscriber) {Subscriber.create()}
   
   
@@ -11,6 +11,11 @@ RSpec.describe Subscriber, type: :model do
     @valid_number = "+1234567890"
     expect(no_number_subscriber).not_to be_valid
     expect(subscriber.number).not_to be_nil
+  end
+
+  it "can subscribe whatsapp numbers" do
+    expect(whatsapp_subscriber).to be_valid
+    expect(whatsapp_subscriber).to be_whatsapp_subscriber
   end
 
 end
