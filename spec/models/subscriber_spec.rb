3ec8f89c07f899ec43738cccefb2c93d692ef819
Yechiel Kalmenson <contact@yechiel.me>
2018-08-01 21:07:01 -0400

    fix the subscriber tests

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -2,22 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
 
-  @valid_number = "+1234567890"
-  let(:subscriber) {Subscriber.create(number: @number)}
+  let(:subscriber) {Subscriber.create(number: @valid_number)}
+  let(:whatsapp_subscriber) {Subscriber.create(number: "invalid_number")}
   let(:no_number_subscriber) {Subscriber.create()}
-
-  let(:invalid_number_subscriber) {Subscriber.create(number: "invalid_number")}
-
+  
+  
   it "has a phone number" do
+    @valid_number = "+1234567890"
     expect(no_number_subscriber).not_to be_valid
     expect(subscriber.number).to eq(@valid_number)
   end
 
-  describe "phone number" do
-
-    it "is a valid number" do
-      expect(invalid_number_subscriber).not_to be_valid
-    end
-
-  end
 end
